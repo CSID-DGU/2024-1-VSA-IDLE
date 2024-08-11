186d26d2bb9760c73d8d56e22a8b85c252ec79dd
Nam-seonwoo <potatomoney716@gmail.com>
2024-08-11 16:58:28 +0900

    [FEAT] : 메인페이지 추가 및 패키지 업데이트

---
+++ ros2_react/src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Container} from "@mui/material";
+
+export default function MainPage() {
+    return (
+        <Container>
+            <h1>Ros2_react</h1>
+            <p>Welcome</p>
+        </Container>
+    );
+}
